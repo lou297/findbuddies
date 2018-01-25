d42d903111b3b3b90f3fc09ed5666aebaa4de505
lou297 <lou297@naver.com>
2018-01-25 22:52:28 +0900

    다중 선택?

--- app/src/main/java/org/capstone/findbuddies/AddGroup.java
+++ app/src/main/java/org/capstone/findbuddies/AddGroup.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddGroup extends AppCompatActivity {
     Buddy.BuddyAdapter adapter;
-    int checked_number=0;
+//    int checked_number=adapter.getCount();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +22,32 @@ public class AddGroup extends AppCompatActivity {
 //        checked.setVisibility(View.VISIBLE);
 
 
-        ListView listview = (ListView)findViewById(R.id.listView);
+        final ListView listview = (ListView)findViewById(R.id.listView);
 
-        adapter = new Buddy.BuddyAdapter();
-        adapter.addbuddy(new BuddyItem("아들","010-1234-1234",R.drawable.boy));
-        adapter.addbuddy(new BuddyItem("딸","010-2345-1234",R.drawable.girl));
-        adapter.addbuddy(new BuddyItem("친구1","010-5324-1234",R.drawable.friend));
-        adapter.addbuddy(new BuddyItem("친구2","010-3424-1234",R.drawable.friend2));
+        final ArrayList<BuddyItem> buddyitem = new ArrayList<BuddyItem>();
+        buddyitem.add(new BuddyItem("아들","010-1234-1234",R.drawable.boy));
+        buddyitem.add(new BuddyItem("아들","010-1234-1234",R.drawable.boy));
+        buddyitem.add(new BuddyItem("아들","010-1234-1234",R.drawable.boy));
+        buddyitem.add(new BuddyItem("아들","010-1234-1234",R.drawable.boy));
+
+
+        final ArrayAdapter<BuddyItem> adapter = new ArrayAdapter<BuddyItem>(this, android.R.layout.simple_list_item_multiple_choice,buddyitem);
+//        adapter = new Buddy.BuddyAdapter();
+//        adapter.addbuddy(new BuddyItem("아들","010-1234-1234",R.drawable.boy));
+//        adapter.addbuddy(new BuddyItem("딸","010-2345-1234",R.drawable.girl));
+//        adapter.addbuddy(new BuddyItem("친구1","010-5324-1234",R.drawable.friend));
+//        adapter.addbuddy(new BuddyItem("친구2","010-3424-1234",R.drawable.friend2));
 
         listview.setAdapter(adapter);
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                checked_number++;
-                Toast toast = Toast.makeText(getApplicationContext(),checked_number+"명 체크됨.",Toast.LENGTH_LONG);
-                toast.show();
+                listview.setItemChecked(position,true);
+                adapter.notifyDataSetChanged();
+//                view.setBackgroundColor();
+//                Toast toast = Toast.makeText(getApplicationContext(),checked_number+"명 체크됨.",Toast.LENGTH_LONG);
+//                toast.show();
 
             }
         });
